540c5a12f53c42c90badbda0ded54194586632b6
robnsiov <robnsiov@gmail.com>
2023-10-15 15:48:25 +0330

    add pages-banner

--- components/user/pages/about-us/index.tsx
+++ components/user/pages/about-us/index.tsx
@@ -2,31 +2,19 @@ import Image from "next/image";
 import AboutUsBanner from "../home/about-us";
 import PrimaryButton from "@/components/shared/buttons/primary";
 import TextInput from "@/components/shared/input/text-input";
+import PagesBanner from "@/components/shared/pages-banner";
 
 const AboutUs = () => {
   return (
     <div className="w-full flex justify-start items-start flex-col">
-      <div className="w-full h-[430px]" style={{ clipPath: " inset(0)" }}>
-        <div
-          className="flex justify-center h-full items-start flex-col container relative z-20 text-white
-        mt-[40px]"
-        >
-          <span className="font-light text-sm mb-2">
-            لورم ایپسوم متن ساختگی با تولید سادگی
-          </span>
-          <h2 className="font-extrabold text-6xl md:text-5xl md:mt-3 leading-[1.4]">
-            تماس با <span className="text-primary">ما</span>
-          </h2>
-        </div>
-        <div className="absolute inset-0 bg-blue-950 opacity-50 z-10"></div>
-        <Image
-          src={"/images/15.jpg"}
-          alt="landing"
-          className="fixed top-0 left-0 w-full h-full object-cover object-center"
-          width={1500}
-          height={800}
-        />
-      </div>
+      <PagesBanner alt="contact-us" src="/images/15.jpg">
+        <span className="font-light text-sm mb-2">
+          لورم ایپسوم متن ساختگی با تولید سادگی
+        </span>
+        <h2 className="font-extrabold text-6xl md:text-5xl md:mt-3 leading-[1.4]">
+          تماس با <span className="text-primary">ما</span>
+        </h2>
+      </PagesBanner>
       <AboutUsBanner />
       <div className="container flex justify-start items-start flex-col mt-6">
         <div className="max-w-md flex justify-start items-start flex-col">
